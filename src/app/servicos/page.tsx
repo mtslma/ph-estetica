ff1b4aa0c2a5ce741d468ed62645d0a285fb46ac
Mateus Lima <Mateus Lima>
2025-07-01 18:39:30 -0300

    Alterações na estrutura geral

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Servicos() {
+    return (
+        <main className="flex items-center w-screen h-screen flex-col justify-center">
+            SERVIÇOS
+            <Link href={"/"}>Página inicial</Link>
+        </main>
+    );
+}
