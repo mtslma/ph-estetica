f88e7728151ed09727166735c19a8b057fea5156
Mateus Lima <Mateus Lima>
2025-07-01 18:44:15 -0300

    Adicionado as configurações do Next e dependências

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ✅ 1. Habilita a exportação para um site 100% estático
+
+  // Export faz o projeto ir para um estático
   output: 'export',
 
-  // ✅ 2. Corrige os caminhos para o GitHub Pages
-  //    Dizendo que seu site vive dentro do subdiretório /ph-estetica
   basePath: '/ph-estetica',
   assetPrefix: '/ph-estetica/',
 
-  // ✅ 3. Mantém a configuração de imagens que você já tinha
   images: {
     unoptimized: true,
-  },
+  }
 };
 
 export default nextConfig;
